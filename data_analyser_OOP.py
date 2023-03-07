875da78d61d924fc1e0cead75da5a011f3787de1
MichaelSc21 <michaelscutariu31@gmail.com>
2023-03-07 21:57:27 +0000

    Updated the file to have the capability to show the price when the mouse hovers over it
    *Note: This only works on PC because PYQT6 is used. Make sure you add a version of this with svg

--- data_analyser_OOP.py
+++ data_analyser_OOP.py
@@ -5,8 +5,9 @@ import numpy as np
 import json
 import sys
 from scipy.optimize import curve_fit, leastsq
+from mplcursors import cursor
 
-
+%matplotlib qt
 
 class big_df():
     def __init__(self, filename, filter_data_bool = False): 
@@ -143,6 +144,45 @@ class small_df():
         for i in range(len(y2)):
             y2[i+offset] = y2[i+offset] + self.y[i]
 
+    def plot_with_hover(self):
+        c = np.random.randint(1,5,size=15)
+        norm = plt.Normalize(1,4)
+        cmap = plt.cm.RdYlGn
+        names = np.array(list("ABCDEFGHIJKLMNO"))
+
+        fig,ax = plt.subplots(figsize = (12, 6))
+        sc = plt.scatter(self.x,self.y)
+
+        annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                            bbox=dict(boxstyle="round", fc="w"),
+                            arrowprops=dict(arrowstyle="->"))
+        annot.set_visible(False)
+
+        def update_annot(ind):
+            
+            pos = sc.get_offsets()[ind["ind"][0]]
+            annot.xy = pos
+            text = "{}".format(" ".join(list(map(str,self.y[ind["ind"]]))))
+            annot.set_text(text)
+            #annot.get_bbox_patch().set_facecolor(cmap(norm(c[ind["ind"][0]])))
+            #annot.get_bbox_patch().set_alpha(0.4)
+            
+
+        def hover(event):
+            vis = annot.get_visible()
+            if event.inaxes == ax:
+                cont, ind = sc.contains(event)
+                if cont:
+                    update_annot(ind)
+                    annot.set_visible(True)
+                    fig.canvas.draw_idle()
+                else:
+                    if vis:
+                        annot.set_visible(False)
+                        fig.canvas.draw_idle()
+
+        fig.canvas.mpl_connect("motion_notify_event", hover)
+        plt.show()
 
 
 
@@ -150,18 +190,19 @@ class small_df():
 
 # %%
 if __name__ == '__main__':
-    df_names = ['IAS_to_LTN_oneway']
-    dict_df = {}
-    small_df = {}
+    df_names = ['OPO_to_BHX_oneway']
+    big_dfs = {}
+    small_dfs = {}
     for df_name in df_names:
         
-        dict_df[df_name] = big_df(filename = df_name+'.json')
+        big_dfs[df_name] = big_df(filename = df_name+'.json', filter_data_bool=True)
 
-        dict_df[df_name].filter_data()
-        temp_df = dict_df[df_name].df
-        small_df[df_name] = dict_df[df_name].small_df(method = 'quantile', quantile = 0.1)
-        fig, ax = plt.subplots(figsize = (12, 6))
-        small_df[df_name].plot_polynomial(degree = 7, ax=None, colour='red')
+        temp_df = big_dfs[df_name].df
+        small_dfs[df_name] = big_dfs[df_name].create_small_df(method = 'quantile', quantile = 0.1)
+        #fig, ax = plt.subplots(figsize = (12, 6))
+        #small_dfs[df_name].plot_polynomial(degree = 7, ax=ax, colour='red')
+
+        small_dfs[df_name].plot_with_hover()
 
 
 # %%
