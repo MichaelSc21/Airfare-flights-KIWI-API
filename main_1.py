7c24e870fd94e16ce6a708ad37264709600c2bf0
MichaelSc21 <michaelscutariu31@gmail.com>
2023-03-19 22:05:21 +0000

    Modified the file to start integrating it into replit

--- main_1.py
+++ main_1.py
@@ -4,6 +4,7 @@ import data_getter_OOP
 import data_analyser_OOP 
 import email_sender
 import API_details
+import time
 reload(data_getter_OOP)
 reload(data_analyser_OOP)
 reload(email_sender)
@@ -14,7 +15,9 @@ Data_getter = data_getter_OOP.Data_getter
 big_df= data_analyser_OOP.big_df
 small_df = data_analyser_OOP.small_df
 if __name__ == '__main__':
+
     #Note: Date is in the format: DD/MM/YYYY
+    
     payload={
     'fly_from': 'LTN',
     'fly_to': 'IAS',
@@ -42,29 +45,32 @@ if __name__ == '__main__':
     'sort':'date',
     'selected_cabins': 'M',
     'limit':1000}
-    data = Data_getter(payload2, sanitise_data = True, delete_data = False)
-    data.using_threads2(dateStart = '20/03/2023',dateEnd = '31/12/2023', max_workers=2, period = 16, max = 1)
+    
+    while True:
+        
+        data = Data_getter(payload2, sanitise_data = True, delete_data = False)
+        #data.using_threads2(dateStart = '20/03/2023',dateEnd = '31/12/2023', max_workers=2, period = 16, max = 1)
 
 
-    json_data = big_df(payload = data.payload,filename = data.filename, filter_data_bool = True)
-    data_analyser = json_data.create_small_df(method = 'quantile', quantile = 0.15)
-    data_analyser.compare_data_small_df(degree = 7, ax=None, colour = 'red')
+        json_data = big_df(payload = data.payload,filename = data.filename, filter_data_bool = True)
+        data_analyser = json_data.create_small_df(method = 'quantile', quantile = 0.15)
+        data_analyser.plot_polynomial_plotly(5)
 
 
-    details_email = dict(        
-        send_from = f"{API_details.EMAIL_USERNAME}", 
-        send_to = f"{API_details.EMAIL_RECIPIENT}", 
-        subject = 'Third attempt at sending an email',
-        files=[data_analyser.file_graph_plotly],
-        username =  f"{API_details.EMAIL_USERNAME}",
-        password = f"{API_details.EMAIL_PASSWORD}",
-        message = 'Lets see if this works',
-        server = "smtp.gmail.com",
-        port = 587
-)
+        details_email = dict(        
+            send_from = f"{API_details.EMAIL_USERNAME}", 
+            send_to = f"{API_details.EMAIL_RECIPIENT}", 
+            subject = 'Third attempt at sending an email',
+            files=[data_analyser.file_graph_plotly],
+            username =  f"{API_details.EMAIL_USERNAME}",
+            password = f"{API_details.EMAIL_PASSWORD}",
+            message = 'Lets see if this works',
+            server = "smtp.gmail.com",
+            port = 587
+    )
 
 
-    email_sender.send_mail(**details_email)
-
+        email_sender.send_mail(**details_email)
+        time.sleep(86400)
 
  # %%
