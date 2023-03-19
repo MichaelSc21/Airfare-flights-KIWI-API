f5c871002c66125efd6ca545e7ec4b88b619c873
MichaelSc21 <michaelscutariu31@gmail.com>
2023-03-19 18:24:59 +0000

    Have finished creating  a file that sends emails about the graphs

--- email_sender.py
+++ email_sender.py
@@ -2,33 +2,102 @@
 from importlib import reload
 import API_details
 reload(API_details)
+import os
 import smtplib
-import mimetypes
-from email.message import EmailMessage
+from pathlib import Path
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from email import encoders
 
 
-def attach_file_to_email(email, filename):
-    """Attach a file identified by filename, to an email message"""
-    with open(filename, 'rb') as f:
-        file_data = f.read()
-        file_type, _ = mimetypes.guess_type(filename)
-        file_type = file_type.split('/', 1)
-        email.add_attachment(file_data, maintype=file_type[0], subtype=file_type[1], filename=filename)
+def send_mail(        
+        send_from = f"{API_details.EMAIL_USERNAME}", 
+        send_to = f"{API_details.EMAIL_RECIPIENT}", 
+        subject = 'Third attempt at sending an email',
+        files=[],
+        username =  f"{API_details.EMAIL_USERNAME}",
+        password = f"{API_details.EMAIL_PASSWORD}",
+        message = 'Lets see if this works',
+        server = "smtp.gmail.com",
+        port = 587):
+    """Compose and send email with provided info and attachments.
 
-msg = EmailMessage()
-msg['Subject'] = 'KIWI API data'
-msg['From'] = f"{API_details.EMAIL_USERNAME}@gmail.com"
-msg['To'] = f"{API_details.EMAIL_RECIPIENT}"
-attach_file_to_email(msg, 'D:\COding\Python\Python web scraping\Flight tickets\Airfare-flights KIWI API\Graphs\Plotly graphs\Test1 Interactive plot.html')
+    Args:
+        send_from (str): from name
+        send_to (list[str]): to name(s)
+        subject (str): message title
+        message (str): message body
+        files (list[str]): list of file paths to be attached to email
+        server (str): mail server host name
+        port (int): port number
+        username (str): server auth username
+        password (str): server auth password
+        use_tls (bool): use TLS mode
+    """
+    #Creating the email message that is going to be sent
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message))
 
-#Sending the message
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+    files_to_be_sent = []
+    if isinstance(files, list):
+        if len(files) == 0:
+            raise KeyError("there is no files passed into the function send_mail in the email_sender file")
+        for i in range(len(files)):
+            files_to_be_sent.append(os.path.join(files[i]))
+    else:
+        files_to_be_sent.append(os.path.join(files))
+
+
+    # attaching the files to the email message 
+    for path in files_to_be_sent:
+        part = MIMEBase('application', "octet-stream")
+        with open(path, 'rb') as file:
+            part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        'attachment; filename={}'.format(Path(path).name))
+        msg.attach(part)
+
+    #Creating a connection with the mail server and sending the email message
+    smtp = smtplib.SMTP(server, port)
     smtp.starttls()
-    smtp.login(f"{API_details.EMAIL_USERNAME}@gmail.com", f"{API_details.EMAIL_PASSWORD}")
+    smtp.login(username, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
 
-    smtp.send_message(msg)
 
 
 
+
+
+if __name__ == '__main__':
+    details_email = dict(        
+        send_from = f"{API_details.EMAIL_USERNAME}", 
+        send_to = f"{API_details.EMAIL_RECIPIENT}", 
+        subject = 'Third attempt at sending an email',
+        files=[API_details.FILE_GRAPH_PLOTLY],
+        username =  f"{API_details.EMAIL_USERNAME}",
+        password = f"{API_details.EMAIL_PASSWORD}",
+        message = 'Lets see if this works',
+        server = "smtp.gmail.com",
+        port = 587
+)
+    send_mail(
+        send_from = f"{API_details.EMAIL_USERNAME}@gmail.com", 
+        send_to = f"{API_details.EMAIL_RECIPIENT}", 
+        subject = 'Third attempt at sending an email',
+        files=[os.path.join('D:/COding/Python/Python web scraping/Flight tickets/Airfare-flights KIWI API/Graphs/Plotly graphs/Test1 Interactive plot.html')],
+        username =  f"{API_details.EMAIL_USERNAME}@gmail.com",
+        password = f"{API_details.EMAIL_PASSWORD}",
+        message = 'Lets see if this works',
+        server = "smtp.gmail.com",
+        port = 587
+        )
 
 # %%
