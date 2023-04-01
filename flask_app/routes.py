aca7a7ddf27a5f948fcf5cd74aadc565becf6165
MichaelSc21 <michaelscutariu31@gmail.com>
2023-04-01 14:03:39 +0100

    updated the setup of the website

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,12 +1,44 @@
+# %%
 from flask_app import app
 from flask import render_template
+
+import sys 
+sys.path.insert(0, 'D:\OneDrive\Coding\A-level\Airfare-flights KIWI API')
 import os
+#sys.path.insert(0, 'D:\OneDrive\Coding\A-level\Airfare-flights KIWI API\Getting_data')
+from Getting_data.data_analyser_OOP import *
+
 
 
 html_graph = os.path.abspath('../../Graphs/Plotly graphs/HTML_graph.html')
 
+current_dir = os.getcwd()
+
+print("Current working directory in routes.py:", current_dir)
+
+@app.route('/chart1')
+def chart1():
+    payload={
+    'fly_from': 'LTN',
+    'fly_to': 'IAS',
+    'date_from': '01/04/2023',
+    'date_to': '16/04/2023',
+    'return_from': '08/04/2023',
+    'return_to': '23/04/2023',
+    'nights_in_dst_from': 7,
+    'nights_in_dst_to': 7,
+    'flight_type': 'round',
+    'adults': '4',
+    'curr': 'GBP',
+    'sort':'date',
+    'selected_cabins': 'M',
+    'limit': 1000}
+    small_df = big_df(payload = payload, filter_data_bool=True).create_small_df(method='quantile', quantile=0.15)
+    small_df.plot_polynomial_plotly(12)
+    json_graph1 = small_df.return_json()
 
 
-@app.route('/')
-def home():
-    return render_template(html_graph)
+    return render_template('chart1.html', json_graph1 = json_graph1)
+
+
+# %%
