7bb26a67baa3a029442336769d4286a4f5cd822f
MichaelSc21 <michaelscutariu31@gmail.com>
2023-04-10 22:39:50 +0100

    Update forms.py

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -14,17 +14,18 @@ class RequiredUnless(InputRequired):
     def __call__(self, form, field):
         other_field_value = form[self.other_field_name].data
         if other_field_value is None:
+
             raise Exception('no field named "%s" in form' % self.other_field)
-        
         # If flight_type is round(so if it is not in values), then this field is required
         print('It got all the way here')
         if other_field_value not in self.values: 
             if self.min!=None:
+                
                 self.number_range = NumberRange(min=self.min)
                 self.number_range.__call__(form, field)
             else:
-                self.stringrequired = InputRequired()
-                self.stringrequired.__call__(form, field)
+                super(RequiredUnless, self).__call__(form, field)
+                
         # If this flight_type is one-way, then this field is optional
         else:
             Optional().__call__(form, field)
@@ -46,11 +47,11 @@ class FlightRequestForm(FlaskForm):
     flight_type = SelectField('Flight Type', choices=[('One-way'), ('Round')], validators=[InputRequired()])
     adults = StringField('Number of Adults', validators=[InputRequired()])
     curr = StringField('Currency', validators=[InputRequired(), Length(max=3)])
-    #sort = StringField('Sort', validators=[InputRequired()])
+    sort = StringField('Sort', validators=[Optional()])
     selected_cabins = SelectField('Selected Cabins', 
                                   choices=[('M', 'M (Economy Class)'), 
                                            ('W', 'W (Economy Premium)'), 
                                            ('C', 'C (Business Class)'), 
                                            ('F', 'F (First Class)')], validators=[InputRequired()])
-    #limit = IntegerField('Limit', validators=[NumberRange(min=0)])
+    limit = IntegerField('Limit', validators=[Optional()])
     submit = SubmitField('Submit')
