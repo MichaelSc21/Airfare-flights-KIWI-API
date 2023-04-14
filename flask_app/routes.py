e6d165409bae62517e3b1ed50c18a7458bd55ef1
MichaelSc21 <michaelscutariu31@gmail.com>
2023-04-14 23:56:01 +0100

    updated functionality to show the graph after the page is loaded, still not finished fully

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,21 +1,23 @@
 import sys 
 import os
-sys.path.insert(0, os.getcwd())
-#sys.path.insert(0, 'D:\OneDrive\Coding\A-level\Airfare-flights KIWI API\Getting_data')from Getting_data.data_analyser_OOP import *
-
-
-import Getting_data.API_details as API_details
+#sys.path.insert(0, os.getcwd())
 
 from flask_wtf.csrf import CSRFProtect
 from flask_app.forms import FlightRequestForm
 from flask_app import app
-from flask import render_template, request, Response
+from flask import render_template, request
+import threading
 
+import Getting_data.API_details as API_details
 import Getting_data.data_getter_OOP as data_getter_OOP
 import Getting_data.data_analyser_OOP as data_analyser_OOP 
+
 Data_getter = data_getter_OOP.Data_getter
 big_df= data_analyser_OOP.big_df
 small_df = data_analyser_OOP.small_df
+
+
+
 
 @app.route('/chart1')
 def chart1():
@@ -77,10 +79,11 @@ def flight_request():
     if form.validate_on_submit():
         #Note: you have to sort out the passing of parameters into the functions
         payload = dict(form.data)
-        payload = payload[:-2]
-        
-        # Redirect to another page or return a response
-        json_graph1 = small_df.return_json()
+        del payload['submit']
+        del payload['csrf_token']
+        print(payload)
+        t1 = threading.Thread(target=get_result)
+        t1.start()
         return render_template('flight_request_data.html')
 
     return render_template('flight_request.html', form=form)
@@ -88,19 +91,41 @@ def flight_request():
 
 @app.route('/get_result')
 def get_result():
-    def generate():
-        getter = Data_getter(payload, 
+    payload={
+    'fly_from': 'LTN',
+    'fly_to': 'IAS',
+    'date_from': '01/04/2023',
+    'date_to': '16/04/2023',
+    'return_from': '08/04/2023',
+    'return_to': '23/04/2023',
+    'nights_in_dst_from': 7,
+    'nights_in_dst_to': 7,
+    'flight_type': 'round',
+    'adults': '4',
+    'curr': 'GBP',
+    'sort':'date',
+    'selected_cabins': 'M',
+    'limit': 1000}
+    getter = Data_getter(payload, 
                         sanitise_data = True, 
                         delete_data = False,
                         dateStart = '01/04/2023',
                         dateEnd = '31/12/2023')
-        big_df = big_df(filename = getter.filename, 
-                                filter_data_bool=True, 
-                                payload = payload,
-                                dateStart = '01/04/2023',
-                                dateEnd = '31/12/2023')
-        
-        small_df = big_df.create_small_df(method = 'quantile', quantile =0.14)
-        small_df.plot_polynomial_plotly(12)
-        yield small_df.return_json()
-    return Response(generate(), mimetype='text/event-stream')
+    print(getter.filename )
+    print(f"""current directory in routes is: + {os.getcwd()}
+    
+    
+    
+    """)
+    big_dfs= big_df(filename = getter.filename, 
+                            filter_data_bool=True, 
+                            payload = payload,
+                            dateStart = '01/04/2023',
+                            dateEnd = '31/12/2023')
+    
+    small_dfs = big_dfs.create_small_df(method = 'quantile', quantile =0.14)
+    small_dfs.plot_polynomial_plotly(12)
+
+    json_graph1 = small_dfs.return_json()
+    print(json_graph1)
+    return {"response": 'True', "data":json_graph1}
