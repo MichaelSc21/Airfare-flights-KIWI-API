bd827dcdde021e9eaeddcd3b846944f2b2d58202
MichaelSc21 <michaelscutariu31@gmail.com>
2023-05-13 20:22:19 +0100

    csrf tokens are now initialised at the top level of the module

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+import Getting_data.API_details as API_details
+
 
 
 app = Flask(__name__)
+app.secret_key = API_details.FORMS_KEY
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 from flask_app import routes
